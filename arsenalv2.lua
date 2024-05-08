69c0bd379d353a014b876a50d5ee44bc3ab6a0c1
alqvriqqq <omerlamaj@icloud.com>
2024-05-08 19:41:11 +0200

    Update arsenalv2.lua

--- arsenalv2.lua
+++ arsenalv2.lua
@@ -55,23 +55,3 @@ coroutine.resume(coroutine.create(function()
         end))
     end
 end))
-
-local esp = Instance.new("BillboardGui")
-esp.Size = UDim2.new(0, 50, 0, 50)
-esp.StudsOffset = Vector3.new(0, 3, 0)
-esp.AlwaysOnTop = true
-
-local espText = Instance.new("TextLabel")
-espText.Parent = esp
-espText.BackgroundTransparency = 1
-espText.Size = UDim2.new(1, 0, 1, 0)
-espText.TextScaled = true
-espText.TextStrokeTransparency = 0.5
-espText.TextColor3 = Color3.fromRGB(255, 255, 255)
-
-for _, v in pairs(game[players]:GetPlayers()) do
-    if v.TeamColor ~= localPlayer.TeamColor then
-        esp.Adornee = v.Character.Head
-        espText.Text = v.Name
-    end
-end
